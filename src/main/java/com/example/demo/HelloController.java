a28e3c695984b2836844b10f0828d8c83e183c44
santhosh202310 <148411343+santhosh202310@users.noreply.github.com>
2023-10-22 21:30:24 -0500

    1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! This is a new line added";
+        return "This is a first springboot app, This is a new line added";
         
     }
 
